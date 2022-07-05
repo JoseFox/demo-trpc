616c2dc30083aebda2171418580040e129274897
JoseFox <foxalejandro60@gmail.com>
2022-07-05 04:23:41 -0500

    added prisma and planetscale

--- src/pages/api/trpc/[trpc].ts
+++ src/pages/api/trpc/[trpc].ts
@@ -1,6 +1,7 @@
 import * as trpc from "@trpc/server";
 import * as trpcNext from "@trpc/server/adapters/next";
 import { z } from "zod";
+import { prisma } from "src/db/client";
 
 export const appRouter = trpc
   .router()
@@ -17,7 +18,18 @@ export const appRouter = trpc
     },
   })
   .query("posts", {
-    resolve() {},
+    async resolve() {
+      const posts = await prisma.post.findMany({});
+      return posts;
+    },
+  })
+  .mutation("createPost", {
+    input: z.object({ title: z.string(), content: z.string() }),
+    async resolve({ input }) {
+      await prisma.post.create({
+        data: { title: input.title, content: input.content },
+      });
+    },
   });
 
 export type AppRouter = typeof appRouter;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,39 @@ import type { NextPage } from "next";
 import { trpc } from "../utils/trpc";
 
 const Home: NextPage = () => {
-  const hello = trpc.useQuery(["hello", { text: "world" }]);
-  if (!hello.data) {
+  const posts = trpc.useQuery(["posts"]);
+  const createPost = trpc.useMutation(["createPost"]);
+  const client = trpc.useContext();
+  console.log(posts);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const title = formData.get("title") as string;
+    const content = formData.get("content") as string;
+    const post = await createPost.mutate({ title, content });
+    client.invalidateQueries(["posts"]);
+  };
+
+  if (!posts.data) {
     return <div>Loading...</div>;
   }
-  return <div className="text-2xl">{hello.data.greeting}</div>;
+  return (
+    <>
+      <div className="text-2xl">Posts: </div>
+      {posts.data.map((post) => (
+        <div key={post.id}>
+          <div className="text-xl">{post.title}</div>
+          <div className="text-sm">{post.content}</div>
+        </div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="title" />
+        <input type="text" name="content" />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 };
 
 export default Home;
